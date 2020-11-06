3fc521b94badca35b89622490b1a6b2038396fcc
Artem Sitnov <stnoov@gmail.com>
2020-11-06 12:15:51 +0200

    LAYOUT: Add basic cards as products examples

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -101,22 +101,74 @@ export default function Main() {
             </Row>
             <Row>
                 <Col>
-                    <Card>
+                    <Card className='mt-3'>
                         <Card.Header className='text-center'><b>Sort by</b></Card.Header>
                         <ListGroup.Item className='text-center'>
                             <input className='m-auto' type='radio' value='name' name="sort-by"/>
-                            <label>Name</label>
+                            <label className='ml-1'>Name</label>
                             <input type='radio' value='price' name="sort-by" className='ml-5'/>
-                            <label>Price</label>
+                            <label className='ml-1'>Price</label>
                         </ListGroup.Item>
                         <Card.Header className='text-center'><b>Price</b> </Card.Header>
                         <ListGroup.Item className='text-center'>
                             <input type='text' className='price-range-input mr-2' placeholder='min'/> – <input type='text' placeholder='max' className='ml-2 price-range-input'/>
                         </ListGroup.Item>
+                        <Card.Header className='text-center'><b>Manufacturer</b> </Card.Header>
+                        <ListGroup.Item className='text-center'>
+                            <div>
+                                <input type='checkbox' /> <label> HP </label>
+                                <input type='checkbox' className='ml-2' /> <label> Intel </label>
+                                <input type='checkbox' className='ml-2' /> <label> AMD </label> <br/>
+                                <input type='checkbox' className='ml-2' /> <label> Lenovo </label>
+                                <input type='checkbox' className='ml-2' /> <label> Samsung </label>
+                            </div>
+                        </ListGroup.Item>
                     </Card>
                 </Col>
-                <Col xs={9}>
-                    Main content
+                <Col xs={9} className='text-center mt-3'>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <Card >
+                                    <Card.Img variant="top" className='p-2' src={Laptop}  style={{ width: '15rem' }} />
+                                    <Card.Body>
+                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                        </Card.Text>
+                                        <Button variant="primary">Go somewhere</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col>
+                                <Card>
+                                    <Card.Img variant="top" className='p-2' src={Laptop} style={{ width: '15rem' }} />
+                                    <Card.Body>
+                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                        </Card.Text>
+                                        <Button variant="primary">Go somewhere</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col>
+                                <Card>
+                                    <Card.Img variant="top" className='p-2' src={Laptop} style={{ width: '15rem' }} />
+                                    <Card.Body>
+                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                        </Card.Text>
+                                        <Button variant="primary">Go somewhere</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Col>
             </Row>
         </Container>
