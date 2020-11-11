a22002874fa3d3c6feb79ad5bc121e4208f40491
Mikael <pursiainen.mikael@gmail.com>
2020-11-11 15:21:27 +0200

    Added logout component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Menu from './components/Menu';
+import Logout from './components/Logout';
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Menu />
+      <Logout />
     </div>
   );
 }
---
+++ client/src/components/Logout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Logout() {
+    return (
+        <div>
+            This is the logout
+        </div>
+    )
+}
