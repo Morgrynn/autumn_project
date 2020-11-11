36f694a17eb014455ece466d1dc2c7c3e0b6e2a2
Anthony <t9haan01@students.oamk.fi>
2020-11-11 17:33:52 +0200

    Fixed error with connection string

--- server/database.js
+++ server/database.js
@@ -13,11 +13,20 @@ dotenv.config({ path: './.env' });
 // database: process.env.DATABASE,
 // password: process.env.DB_PASS,
 
+// const connection = new Pool({
+//   user: process.env.HEROKU_USER,
+//   host: process.env.HEROKU_HOST,
+//   database: process.env.HEROKU_DB,
+//   password: process.env.HEROKU_PASS,
+//   port: process.env.DB_PORT,
+//   ssl: true,
+// });
+
 const connection = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DATABASE,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  connectionString: process.env.HEROKU_URI,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 module.exports = connection;
