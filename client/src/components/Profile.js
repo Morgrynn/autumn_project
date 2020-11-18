8ea6d1f2ba6472a827fecac2be3b9c404a4f7a73
Artem Sitnov <stnoov@gmail.com>
2020-11-18 17:54:49 +0200

    Add profile forms

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,21 +1,55 @@
 import React from "react";
-import {Modal, Button} from "react-bootstrap";
+import {Modal, Button, Container, Col, Row, Form} from "react-bootstrap";
 
 export default function ProfileModal(props) {
     return (
-        <Modal show={props.showProfile} onHide={props.handleClose} animation={false}>
+        <Modal show={props.showProfile} onHide={props.handleClose} size="lg">
             <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
+                <Modal.Title>User profile</Modal.Title>
             </Modal.Header>
-            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={props.handleClose}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={props.handleClose}>
-                    Save Changes
-                </Button>
-            </Modal.Footer>
+            <Modal.Body>
+                <Container>
+                    <Row className='text-center'>
+                        <Col>
+                                <h5>Change password</h5>
+                            <Form.Group>
+                                <br/>
+                                <Form.Control size="sm" type="text" placeholder="Old password" />
+                                <br/>
+                                <Form.Control size="sm" type="text" placeholder="New password" />
+                                <br/>
+                                <Button className='float-right'>Save</Button>
+
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                                <h5>Change email</h5>
+                                <Form.Group>
+                                    <br/>
+                                    <Form.Control size="sm" type="text" placeholder={props.currentUser.email} />
+                                    <br/>
+                                    <Button className='float-right'>Save</Button>
+
+                                </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row className='text-center'>
+                        <Col>
+
+                        </Col>
+                        <Col>
+                            <h5>Add balance</h5>
+                            <Form.Group>
+                                <br/>
+                                <Form.Control size="sm" type="text" placeholder='Amount' />
+                                <br/>
+                                <Button className='float-right'>Pay</Button>
+
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                </Container>
+            </Modal.Body>
         </Modal>
     )
 }
