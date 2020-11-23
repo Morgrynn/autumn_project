473e11e2092f8716385e92dc7f0144ca686add8d
Anthony <t9haan01@students.oamk.fi>
2020-11-23 21:49:05 +0200

    Setting up search function

--- client/src/components/Trending.js
+++ client/src/components/Trending.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Container, Table, Image, Row, Col, Button } from 'react-bootstrap';
 
-export default function Home() {
+export default function Home({ products, baseUrl }) {
   return (
     <Container className='mt-5'>
       <Row>
         <Col></Col>
         <Col xs={9}>
           <div className='main-title text-center'>Trending</div>
+          <Table responsive hover>
+            <tbody>
+              {/* {console.log('Products: ', typeof products)} */}
+              {/* {products.map((product, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <Container>
+                        <Row>
+                          <Col style={{ width: '171px' }}>
+                            <Image
+                              src={`${baseUrl}${product.image}`}
+                              thumbnail
+                              style={{ maxWidth: '75%' }}
+                            />
+                          </Col>
+                        </Row>
+                      </Container>
+                    </td>
+                  </tr>
+                );
+              })} */}
+            </tbody>
+          </Table>
         </Col>
         <Col></Col>
       </Row>
