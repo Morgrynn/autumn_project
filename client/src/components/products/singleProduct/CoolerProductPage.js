9bfec7cb58a408a61f0f5f0f47171fb58fc20b69
Anthony <t9haan01@students.oamk.fi>
2020-11-26 10:56:40 +0200

    Update to product descriptions

--- client/src/components/products/singleProduct/CoolerProductPage.js
+++ client/src/components/products/singleProduct/CoolerProductPage.js
@@ -1,31 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
 import {
-    Container,
-    Card,
-    ListGroup,
-    Button,
-    Image,
-    Row,
-    Col,
-    ListGroupItem,
-  } from 'react-bootstrap';
+  Container,
+  ListGroup,
+  Image,
+  Row,
+  Col,
+  ListGroupItem,
+  Tabs,
+  Tab,
+} from 'react-bootstrap';
+import AddToCart from './AddToCart';
+import DescriptionList from './DescriptionList';
+import UserRatings from './UserRatings';
 
-export default function CoolerProductPage({ item, baseUrl}) {
-    return (
-        <Container>
+export default function CoolerProductPage({ item, baseUrl, addItem }) {
+  const [key, setKey] = useState('product');
+
+  return (
+    <Container style={{ marginTop: '10px' }}>
       <Row>
-      <Col md={{ span: 6, offset: 4}}>
-      <Card border="secondary" style={{ width: '18rem' }}>
-        <Card.Title style={{ textAlign: 'center' }}>{item.cooler_name}</Card.Title>
-        <Card.Img variant='top' src={`${baseUrl}${item.image}`} />
-        <ListGroup className="list-group-flush">
-            <ListGroupItem>{item.fan_rpm}</ListGroupItem>
+        <Col xs={6} md={4}>
+          <div>
+            <Image fluid src={`${baseUrl}${item.image}`} />
+          </div>
+        </Col>
+        <Col xs={12} md={8}>
+          <ListGroup className='list-group-flush'>
+            <div>
+              <ListGroupItem style={{ textAlign: 'center' }}>
+                {item.name}
+              </ListGroupItem>
+            </div>
+            <ListGroupItem>{item.fan_rpm} Cores</ListGroupItem>
             <ListGroupItem>{item.noise_level}</ListGroupItem>
             <ListGroupItem>€{item.price}</ListGroupItem>
-        </ListGroup>
-      </Card>
-      </Col>
-    </Row>
+          </ListGroup>
+          <AddToCart item={item} addItem={addItem} />
+        </Col>
+      </Row>
+      <Row style={{ marginTop: '5px' }}>
+        <Col>
+          <Tabs activeKey={key} onSelect={(k) => setKey(k)}>
+            <Tab eventKey='product' title='Product Description'>
+              <DescriptionList />
+            </Tab>
+            <Tab eventKey='ratings' title='Ratings & Reviews'>
+              <UserRatings />
+            </Tab>
+          </Tabs>
+        </Col>
+      </Row>
     </Container>
-    )
+  );
 }
